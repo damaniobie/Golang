893729466d89f7c98f78bc09f10f2f96a1519034
damaniobie <35574248+damaniobie@users.noreply.github.com>
2018-03-11 20:59:33 -0400

    Update practice1.go

--- practice1.go
+++ practice1.go
@@ -1,54 +1,3 @@
-The Go PlaygroundRun  Format   Imports  Share About
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
 
 package main
 
@@ -62,6 +11,10 @@ func main() {
 	arr[i] = i*5
 	}
 	fmt.Println(arr)
+	
+	for i,j := 0,0;i<5;i, j = i+1,j+2{	//initialize two vars in a for loop
+		fmt.Println(i)	
+	}
 
 	/*kvs := map[string]string{"a": "apple", "b": "banana"}
     for k, v := range kvs {
